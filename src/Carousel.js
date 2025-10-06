d40f2f6a368fd742dab395b4dc17a787d261ba75
ckyever <ckyeverg@gmail.com>
2025-10-06 22:34:46 +1100

    Add buttons to go next/previous

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,3 +1,6 @@
+import leftChevron from "./assets/chevron-left.svg";
+import rightChevron from "./assets/chevron-right.svg";
+
 export class Carousel {
   static visibleClassName = "visible";
 
@@ -8,6 +11,18 @@ export class Carousel {
     if (this.images.length > 0) {
       this.carouselElement = document.createElement("div");
       this.carouselElement.classList = "carousel";
+
+      const previousButton = document.createElement("button");
+      previousButton.classList = "control previous";
+      const previousIcon = document.createElement("img");
+      previousIcon.src = leftChevron;
+      previousIcon.classList = "chevron";
+      previousButton.addEventListener("click", () => {
+        this.previous();
+      });
+      previousButton.appendChild(previousIcon);
+      this.carouselElement.appendChild(previousButton);
+
       this.images.forEach((image, index) => {
         const imageElement = document.createElement("img");
         imageElement.src = image;
@@ -21,6 +36,17 @@ export class Carousel {
         this.carouselElement.appendChild(imageElement);
       });
       container.appendChild(this.carouselElement);
+
+      const nextButton = document.createElement("button");
+      nextButton.classList = "control next";
+      const nextIcon = document.createElement("img");
+      nextIcon.src = rightChevron;
+      nextIcon.classList = "chevron";
+      nextButton.appendChild(nextIcon);
+      nextButton.addEventListener("click", () => {
+        this.next();
+      });
+      this.carouselElement.appendChild(nextButton);
     }
 
     this.carouselImageList = this.carouselElement.querySelectorAll("img.slide");
